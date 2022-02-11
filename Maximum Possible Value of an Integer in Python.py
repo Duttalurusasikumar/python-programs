2500c4269597d9a3fc5695e5317014765f7418e1
Duttaluru sasikumar <64832509+Duttalurusasikumar@users.noreply.github.com>
2022-02-11 09:44:32 +0530

    Add files via upload

---
+++ Maximum Possible Value of an Integer in Python.py
@@ -0,0 +1,7 @@
+# A Python program to display that we can store  
+# large numbers in Python  
+  
+a = 10000000000000000000000000000000000000000000  
+a = a + 1  
+print(type(a))  
+print (a)      
